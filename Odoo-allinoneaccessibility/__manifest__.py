9314d061bb22cef295ae187edab81ff6d5409898
Skynet Technologies <developer3@skynettechnologies.com>
2024-08-29 10:32:51 +0530

    Update __manifest__.py

--- Odoo-allinoneaccessibility/__manifest__.py
+++ Odoo-allinoneaccessibility/__manifest__.py
@@ -2,7 +2,7 @@
 {
     'name': 'All in One Accessibility',
     'category': "Website",
-    'version': '1.0',
+    'version': '1.1',
     'license': 'OPL-1',
     'summary': 'All in One Accessibility widget is based on assistive technology and AI that helps organizations enhance the accessibility and usability of their website quickly',
     'description': 'All in One Accessibility widget improves Odoo website ADA compliance and browser experience for ADA, WCAG 2.1, Section 508, Australian DDA, European EAA EN 301 549, UK Equality Act (EA), Israeli Standard 5568, California Unruh, Ontario AODA, Canada ACA, German BITV, and France RGAA Standards',

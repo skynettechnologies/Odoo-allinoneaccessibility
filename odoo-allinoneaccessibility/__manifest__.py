4daa362f2d35dce375bb1e35110d1a583f7b730a
Skynet Technologies <developer3@skynettechnologies.com>
2026-03-12 19:11:49 +0530

    Solved import issues on version 18

--- odoo-allinoneaccessibility/__manifest__.py
+++ odoo-allinoneaccessibility/__manifest__.py
@@ -1,25 +1,24 @@
 # -*- encoding: utf-8 -*-
 {
     'name': 'All in One Accessibility',
-    'category': "Website",
-    'version': '1.1',
+    'category': 'Website',
+    'version': '1.2',
     'license': 'OPL-1',
-    'summary': 'All in One Accessibility widget is based on assistive technology and AI that helps organizations enhance the accessibility and usability of their website quickly',
-    'description': 'All in One Accessibility widget improves Odoo website ADA compliance and browser experience for ADA, WCAG 2.1, Section 508, Australian DDA, European EAA EN 301 549, UK Equality Act (EA), Israeli Standard 5568, California Unruh, Ontario AODA, Canada ACA, German BITV, and France RGAA Standards',
+    'summary': 'All in One Accessibility widget helps organizations enhance accessibility and usability of their website',
+    'description': 'All in One Accessibility widget improves Odoo website ADA compliance for WCAG 2.1, Section 508, Australian DDA, European EAA EN 301 549, UK Equality Act, Israeli Standard 5568, California Unruh, Ontario AODA, Canada ACA, German BITV, France RGAA Standards',
     'author': 'Skynet Technologies USA LLC',
-    'website':'https://www.skynettechnologies.com/all-in-one-accessibility',
-    'depends': ['base', 'base_setup', 'web'],
+    'website': 'https://www.skynettechnologies.com/all-in-one-accessibility',
+    'depends': ['base', 'base_setup', 'web', 'website'],
     'data': [
-        # 'security/ir.model.access.csv',
-        'views/res_config_settings_views.xml',
+        'models/aioa_model.xml',
+        'security/ir.model.access.csv',
+        'actions/aioa_actions.xml',
+        'views/aioa_views.xml',
         'templates/base_url_passing.xml',
-
-    ],
-    'qweb': [
     ],
     'images': ['static/description/banner.jpg'],
     'installable': True,
     'application': True,
-    'support':'hello@skynettechnologies.com',
+    'auto_install': False,
+    'support': 'hello@skynettechnologies.com',
 }
-
